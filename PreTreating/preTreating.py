333b86a93708672114c7836e82ee9a9bceaebdf0
FengFan <ffeng1017@gmail.com>
2018-03-11 20:13:31 +0800

    update

--- PreTreating/preTreating.py
+++ PreTreating/preTreating.py
@@ -1,4 +1,5 @@
 import math
+import numpy as np
 
 class preTreating:
     dataFilePath = ""
@@ -24,8 +25,8 @@ class preTreating:
     # ==============================
     # 功能：数据归一化
     # 任务：杂波去除、航迹预测
-    # 作者：
-    # 修改日期：
+    # 作者：冯帆
+    # 修改日期：2018-3-8
     # ==============================
     def normalization(self, f_range):
         # f_range:[[a, b], [-1], [...]...]
@@ -33,8 +34,19 @@ class preTreating:
         # if the corresponding feature needs to be normalization, the index element is [a, b]
         # else, the element is [-1]
         # ! the f_range lists is read from the GUI
+        print(">>> Start normalization for \nfeatures...", f_range.length)
+        f_range = [a,b]
+        for i in range(len(self.data)):
+            for j in range(len(self.data[i])):
+                arr = np.array(self.data[i])
+                maxValue = arr.max()
+                minValue = arr.min()
+                k = (b-a)/(maxValue-minValue)
+                norX = k*(float(self.data[i][j])-minValue)+a
+                self.data[i][j] = str('{:.2f}'.format(norX))
+        print("Normalization done...")
         return
-
+     
     # ==============================
     # 功能：将极坐标修改为直角坐标
     # 任务：航迹预测
@@ -53,13 +65,34 @@ class preTreating:
         return
 
     # ==============================
-    # 功能：数据扩充
+    # 功能：数据扩充，对样本数进行统计，少例样本扩充
     # 任务：杂波去除
-    # 作者：
-    # 修改日期：
+    # 作者：冯帆
+    # 修改日期：2018-3-8
     # ==============================
     def expanding_1(self):
+        len_ = len(self.data)
+        print(">>> Start expanding...\noriginal length: ", len_)
+        expanded_data = []
+        raw_data = np.array(self.data)
+        noise_num = sum(raw_data[:,7] == '-1')
+        signal_num = sum(raw_data[:,7] == '1')
+        
+        if(noise_num > signal_num):
+            for index in range(int(round(noise_num/signal_num))):
+                for item in raw_data[raw_data[:,7] == '1'].copy():
+                    expanded_data.append(item.tolist()) 
+            for item in raw_data[raw_data[:,7] == '-1'].copy():
+                expanded_data.append(item.tolist())
+        if(noise_num < signal_num):
+            for index in range(int(round(signal_num/noise_num))):
+                for item in raw_data[raw_data[:,7] == '-1'].copy():
+                    expanded_data.append(item.tolist())
+            for item in raw_data[raw_data[:,7] == '1'].copy():
+                expanded_data.append(item.tolist())
+        self.data = expanded_data
         return
+
 
     # ==============================
     # 功能：数据扩充：通过旋转航迹的方法扩充航迹数据
